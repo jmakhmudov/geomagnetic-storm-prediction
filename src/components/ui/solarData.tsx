38f1f66fd2c45441663c8105a59ad83cdc88b8a2
jmakhmudov <makhmudov@tuta.io>
2023-10-04 21:58:53 +0500

    fix key

--- src/components/ui/solarData.tsx
+++ src/components/ui/solarData.tsx
@@ -1,20 +1,24 @@
-import { 
+import {
     ArrowUp,
     ArrowDown,
     Minus
 } from "react-feather";
 
 interface SolarProps {
-    title: string,
-    data: string,
-    trend: 0 | 1 | 2, //0-same 1-down 2-up
-    changed: number
+    title: string;
+    data: string;
+    trend: 0 | 1 | 2; // 0-same 1-down 2-up
+    changed: number;
 }
 
-const arrows = [<Minus className="text-gray-400" />, <ArrowDown className="text-red-700" />, <ArrowUp className="text-green-400" />]
+const arrows = [
+    <Minus key="minus" className="text-gray-400" />,
+    <ArrowDown key="arrow-down" className="text-red-700" />,
+    <ArrowUp key="arrow-up" className="text-green-400" />
+];
 
-const sign = ["", "-", "+"]
-const colors = ["", "text-red-700", "text-green-400"]
+const sign = ["", "-", "+"];
+const colors = ["", "text-red-700", "text-green-400"];
 
 const SolarData = ({ title, data, trend, changed }: SolarProps) => {
 
@@ -23,7 +27,6 @@ const SolarData = ({ title, data, trend, changed }: SolarProps) => {
             <div>{title}</div>
             <section className="flex items-center">
                 <div className="text-5xl font-extrabold">{data}</div>
-
                 {arrows[trend]}
                 <div className={`${colors[trend]} text-lg`}>{sign[trend]} {changed}</div>
             </section>
